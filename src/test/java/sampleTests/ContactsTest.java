5a45391d248294b3aaf9a775bfdda252c8814d4b
Pawan <Pawan@DESKTOP-1H1R9GO>
2022-09-12 16:29:54 +0530

    commit and push by user 1

---
+++ src/test/java/sampleTests/ContactsTest.java
@@ -0,0 +1,26 @@
+package sampleTests;
+
+import org.testng.annotations.Test;
+
+public class ContactsTest {
+	@Test
+	public void createContact()
+	{
+		System.out.println(" contact is created");
+
+	}
+	@Test
+	public void modifyContact()
+	{
+		System.out.println("contact is modified");
+
+	}
+	@Test
+		public void deleteContact()
+	{
+		System.out.println("contact is deleted");
+
+	}
+
+
+}
---
+++ src/test/java/sampleTests/OrganisationTest.java
@@ -0,0 +1,24 @@
+package sampleTests;
+
+import org.testng.annotations.Test;
+
+public class OrganisationTest {
+	@Test
+	public void createorganisation()
+	{
+		System.out.println("org is created");
+
+	}
+	@Test
+	public void modifyOrganisation()
+	{
+		System.out.println("org is modified");
+
+	}
+	@Test
+		public void deleteorganisation()
+	{
+		System.out.println("org is deleted");
+
+	}
+}
